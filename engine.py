ace71c28eab701ac43a4f0b70f5b61a3b1dac1ac
superspeeder <andy@delusion.org>
2019-02-13 12:10:05 -0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -4,3 +4,26 @@ from pygame.locals import *
 Sprite = pygame.sprite.Sprite
 Group = pygame.sprite.Group
 
+def BaseSprite(Sprite):
+  def __init__(self, **kwargs):
+    self.fillAttribs(**kwargs)
+  
+  def fillAttribs(**kwargs):
+    image_type = -1
+    for key, val in kwargs.items():
+      if BaseSprite.attribAllowed(key):
+        cat = BaseSprite.getAttribCategory(key)
+        if cat == BaseSprite.CATEGORY_MAIN_VISUAL:
+          if not isinstance(image, pygame.Surface):
+            print("[Error] image mode true but specified image is not a pygame surface")
+          if key == "image" and image_type != 1:
+            self.image = image
+            
+          
+          if key == "color" and image_type != 0:
+            if "size" in kwargs:
+              surf = pygame.Surface(kwargs[size])
+            else:
+              print("[Error] No texture size specified and fill color mode is true")
+    
+def generateSprite(
